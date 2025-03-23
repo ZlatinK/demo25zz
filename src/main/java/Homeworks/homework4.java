f7489bc5503e58ab0900d07bbcd96c0cbb46422f
ZlatinK <zlatinkostadinow@gmail.com>
2025-03-23 17:47:27 +0200

    Some changes

--- src/main/java/Homeworks/homework4.java
+++ src/main/java/Homeworks/homework4.java
@@ -58,8 +58,10 @@ public class homework4 {
         System.out.println();
     }
     public static int sumFibonachi(int N) {
-        int a = 0, b = 1, sum = 0;
-        for (int i = 0; i < N; i++) {
+        if (N <= 0) return 0;
+
+        int a = 0, b = 1, sum = 1;
+        for (int i = 1; i < N; i++) {
             sum += a;
             int next = a + b;
             a = b;
