e4e64509e07d6fd76ac936d9a5b1138d96c5aa6b
ZlatinK <zlatinkostadinow@gmail.com>
2025-05-09 19:17:43 +0300

    new lesson

---
+++ src/main/java/exceptionsDemo/Test.java
@@ -0,0 +1,9 @@
+package exceptionsDemo;
+import java.lang.Throwable;
+import java.io.IOException;
+
+public class Test {
+    public static void main(String[] args) throws IOException {
+        FileReader.readFile("./src/main/java/resources/hello.txt");
+    }
+}
